509877b9b4be4f8f7b23571880036eb34e1c6206
moody-jnan <jithya.n@outlook.com>
2022-02-20 22:28:40 +1100

    Completed bot manager POC. Added cron job to bot for registration

--- bot/src/controlServer.ts
+++ bot/src/controlServer.ts
@@ -1,12 +1,39 @@
 import express from "express";
 import { Config } from "./config.js";
 import { generalLogger } from "./log/index.js";
+import axios, { AxiosError } from "axios";
+import cron from "node-cron";
 
 const controlServer = express();
 
 export const SERVER_CONTROL = {
   shutdown: false,
 };
+
+async function registerWithBotManager(): Promise<void> {
+  return axios
+    .post("http://localhost:8900/register", {
+      version: Config.APP_VERSION,
+      location: Config.APPSTATE_FILENAME,
+      exchange: Config.EXCHANGE,
+      port: Config.PORT,
+      symbol: Config.SYMBOL,
+    })
+    .then(() => {
+      generalLogger.info("Sent registration to manager");
+    })
+    .catch((e) => {
+      const { message, code, response, config } = e as AxiosError;
+      generalLogger.error("Manager registration failed", {
+        message,
+        code,
+        response: response?.data,
+        status: response?.status,
+        statusText: response?.statusText,
+        config,
+      });
+    });
+}
 
 controlServer.post("/shutdown", (req, res, next) => {
   generalLogger.info("Server shutdown request received, initiating...");
@@ -21,5 +48,7 @@ export function startControlServer() {
   return controlServer.listen(port, () => {
     console.log("Started listening on port " + port);
     generalLogger.info("Started control server on port " + port);
+    registerWithBotManager();
+    cron.schedule("0 0 1 * *", registerWithBotManager);
   });
 }
--- manager/src/index.ts
+++ manager/src/index.ts
@@ -1,3 +1,80 @@
 import express from "express";
+import { z } from "zod";
+import farmhash from "farmhash";
+import { request } from "gaxios";
+
+const SEED = 10_240;
+
+const BotInfoReq = z.object({
+  version: z.string(),
+  exchange: z.string(),
+  symbol: z.string(),
+  port: z.string(),
+  location: z.string().optional(),
+});
+
+type TBotInfoReq = z.infer<typeof BotInfoReq>;
+
+interface IBotInfo extends TBotInfoReq {
+  status: "HEALTHY" | "MIA" | "SHUTDOWN";
+  hostname: string;
+}
+
+export const botRegister: Record<string, IBotInfo> = {};
+
+function getIdFromData(botInfo: TBotInfoReq): string {
+  return farmhash.hash64WithSeed(
+    Buffer.from(
+      [botInfo.exchange, botInfo.port, botInfo.symbol].join(":"),
+      "utf-8"
+    ),
+    SEED
+  );
+}
 
 const server = express();
+server.use(express.json());
+
+server.post("/register", (req, res) => {
+  const botInfo = BotInfoReq.parse(req.body);
+  const id = getIdFromData(botInfo);
+
+  const data: IBotInfo = {
+    ...botInfo,
+    hostname: req.hostname,
+    status: "HEALTHY",
+  };
+
+  console.log("Received registration", { ...data, id });
+  botRegister[id] = data;
+
+  return res.status(201).json({ status: "SUCCESS" });
+});
+
+server.get("/bots", (req, res) =>
+  res.json(
+    Object.keys(botRegister).map((id) => ({ id, ...(botRegister[id] ?? {}) }))
+  )
+);
+
+const ShutdownRequest = z.object({ id: z.string() });
+
+server.post("/bots/shutdown", async (req, res) => {
+  const { id } = ShutdownRequest.parse(req.body);
+
+  if (!id || !botRegister.hasOwnProperty(id)) {
+    return res.status(404).json({ status: "NOT FOUND", id });
+  } else {
+    const { port, hostname } = botRegister[id];
+    await request({
+      baseURL: `http://${hostname}:${port}`,
+      url: `/shutdown`,
+    });
+    botRegister[id].status = "SHUTDOWN";
+    return res.json({ status: "OK" });
+  }
+});
+
+server.listen(8900, () => {
+  console.log("Listening on port" + 8900);
+});
