242a6cc06f4f5c9bf2d2155eb0c57df11a25ef1d
moody-jnan <jithya.n@outlook.com>
2022-02-20 23:43:34 +1100

    Quick build script for manager

---
+++ manager/esbuild.config.cjs
@@ -0,0 +1,39 @@
+const { build } = require("esbuild");
+const revision =
+  require("child_process")
+    .execSync("git rev-parse HEAD")
+    .toString()
+    .trim()
+    .slice(0, 7) || "";
+
+// const define = {
+//   "process.env.COLLECT_PRICE_STATS": `"${process.env.COLLECT_PRICE_STATS}"`,
+//   "process.env.NODE_ENV": `"${process.env.NODE_ENV}"`,
+//   "process.env.APP_VERSION": `"${revision}"`,
+//   "process.env.BINANCE_KEY": `"${process.env.BINANCE_KEY}"`,
+//   "process.env.BINANCE_SECRET": `"${process.env.BINANCE_SECRET}"`,
+//   "process.env.STABLE_COIN": `"${process.env.STABLE_COIN}"`,
+//   "process.env.VOLATILE_COIN": `"${process.env.VOLATILE_COIN}"`,
+//   "process.env.EXCHANGE": `"${process.env.EXCHANGE}"`,
+//   "process.env.MAX_BUY_AMOUNT": `"${process.env.MAX_BUY_AMOUNT}"`,
+//   "process.env.PORT": `"${process.env.PORT}"`,
+// };
+
+console.log("Starting build version", revision);
+// console.log("ENV", define);
+
+build({
+  loader: { ".node": "file" },
+  entryPoints: ["./src/index.ts"],
+  bundle: true,
+  minify: true,
+  outfile: "dist/index.js",
+  platform: "node",
+  target: "node17.4",
+  treeShaking: true,
+  // NOTE: Esbuild has a bug when outputting to ESM
+  // Need to keep tabs on if it gets fixed
+  // https://github.com/evanw/esbuild/issues/1921
+  format: "cjs",
+  // define,
+});
