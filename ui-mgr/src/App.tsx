ba694613175405d6e229b9fb179418d156e410e7
moody-jnan <jithya.n@outlook.com>
2022-02-21 23:17:56 +1100

    Initial commit - Bot manager UI

---
+++ ui-mgr/src/App.tsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+
+interface AppProps {}
+
+function App({}: AppProps) {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
