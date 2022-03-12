f08bc3d4ffda8b28d0015ad366d09a29a0f7aaaa
moody-jnan <jithya.n@outlook.com>
2022-03-12 21:47:23 +1100

    Switch event-stream for rxjs rxnode

--- analysis/src/index.ts
+++ analysis/src/index.ts
@@ -1,10 +1,22 @@
 import { writeApiPricesForSymbol } from "./parse/api";
 import { startSimulations } from "./simulation";
+import fs from "fs";
+import es from "event-stream";
 
+import { readFile, writeFile } from "@rxnode/fs";
+import { switchMap } from "rxjs/operators";
+
+readFile("src/some-file.js")
+  .pipe(switchMap((data) => writeFile("src/some-file.js", data)))
+  .subscribe({
+    complete() {
+      console.log("Complete!");
+    },
+  });
 // writeApiPricesForSymbol("lunabusd");
 
-console.time("simul");
-startSimulations(2, "luna").then((res) => {
-  console.timeLog("simul", res[0]);
-  console.timeEnd("simul");
-});
+// console.time("simul");
+// startSimulations(2, "luna").then((res) => {
+//   console.timeLog("simul", res[0]);
+//   console.timeEnd("simul");
+// });
