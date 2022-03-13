9667824d6b21029afb9493090d84b46c0ded8145
moody-jnan <jithya.n@outlook.com>
2022-03-13 21:09:20 +1100

    Improved analysis pulldown script

--- analysis/scripts/pulldown.mjs
+++ analysis/scripts/pulldown.mjs
@@ -5,9 +5,11 @@ const symbol = (await question("What bot symbol you after? ")).trim();
 const localdir = `./data/${symbol}`;
 await $`mkdir ${localdir}`;
 
-await $`gcloud compute scp --zone=asia-northeast1-b jithya_n@instance-1:~/bots/${symbol}/*.csv ${localdir}`;
-await $`gcloud compute scp --zone=asia-northeast1-b jithya_n@instance-1:~/bots/${symbol}/*-api.log ${localdir}`;
-await $`gcloud compute scp --zone=asia-northeast1-b jithya_n@instance-1:~/bots/${symbol}/*.csv.gz ${localdir}`;
-await $`gcloud compute scp --zone=asia-northeast1-b jithya_n@instance-1:~/bots/${symbol}/*-api.log.gz ${localdir}`;
+if (symbol === "pricebot") {
+  await $`gcloud compute scp --zone=asia-northeast1-b jithya_n@instance-1:~/bots/${symbol}/*-api.lo* ${localdir}`;
+} else {
+  await $`gcloud compute scp --zone=asia-northeast1-b jithya_n@instance-1:~/bots/${symbol}/*.cs* ${localdir}`;
+  await $`gcloud compute scp --zone=asia-northeast1-b jithya_n@instance-1:~/bots/${symbol}/*-api.lo* ${localdir}`;
+}
 
 await $`gzip -d ${localdir}/*.gz`;
