e25ede0671d47ed81171b0beaf86ccbca48d69ad
moody-jnan <jithya.n@outlook.com>
2022-03-14 23:12:31 +1100

    Putting in undefined checks and logging to manager

--- manager/src/tasks/checkStatus.ts
+++ manager/src/tasks/checkStatus.ts
@@ -12,16 +12,26 @@ function isLastCheckInOverAnHourAgo(lastCheckIn: Date) {
 const checkBotStatus = () => {
   Object.keys(botRegister).forEach((botId) => {
     const currentBot = botRegister.state[botId];
-    const { status } = currentBot;
-    const botIsDown = isLastCheckInOverAnHourAgo(currentBot.lastCheckIn);
-    const flagError = botIsDown && ["ONLINE", "SHUTTING DOWN"].includes(status);
+    if (currentBot) {
+      const { status } = currentBot;
+      const botIsDown = isLastCheckInOverAnHourAgo(currentBot.lastCheckIn);
+      const flagError =
+        botIsDown && ["ONLINE", "SHUTTING DOWN"].includes(status);
+      logger.info("Checking bot status", { botIsDown, currentBot, flagError });
 
-    if (flagError) {
-      logger.error("Bot has not sent a health check in the last hour", {
-        ...currentBot,
+      if (flagError) {
+        logger.error("Bot has not sent a health check in the last hour", {
+          ...currentBot,
+          botId,
+        });
+        currentBot.status = "NOT WORKING";
+      }
+    } else {
+      logger.warn("currentBot does not exist", {
+        currentBot,
         botId,
+        state: botRegister.state,
       });
-      currentBot.status = "NOT WORKING";
     }
   });
 };
