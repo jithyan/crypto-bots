5e757f925fbdf61b3c84acbbf33c1f8c26bdf64b
moody-jnan <jithya.n@outlook.com>
2022-04-11 20:22:51 +1000

    Reduce BICO $, tweak httpServer rate limit

--- manager/src/httpServer.ts
+++ manager/src/httpServer.ts
@@ -37,7 +37,7 @@ import {
 
 const apiLimiter = rateLimit({
   windowMs: 3 * 60 * 1000, // 10 minutes
-  max: 100, // Limit each IP to 100 requests per `window` (here, per 10 minutes)
+  max: 300, // Limit each IP to 100 requests per `window` (here, per 10 minutes)
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
